ff9436dac01d917cb509f83342e5a0f9e862f6c9
danroad <56237641+danroad@users.noreply.github.com>
2019-10-06 23:30:51 -0300

    Inclusão da Entidade : Vendedor e NOta
    Seque atualização com a criação de entidades e chaves estrangeiras.

--- teste1.sql
+++ teste1.sql
@@ -14,4 +14,18 @@ valor int,
 id_produto int primary key);
 
 create table Vendedor(
+nome varchar(50),
+cpf varchar(50),
+matricula int,
+id_vendedor int auto_increment primary key);
+
+create table nota (
+data1 char(5),
+hora char(5),
+cliente int,
+produto int,
+vendedor int,
+constraint c1 foreign key (cliente) references Cliente(id_cliente),
+constraint p1 foreign key (produto) references produto(id_produto),
+constraint v1 foreign key (vendedor) references Vendedor(id_vendedor));
 
